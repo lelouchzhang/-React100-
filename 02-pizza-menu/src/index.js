dc12b5a6c02e01a8ddb290fadab4bfc4ca70ef8b
lelouchZhang <zhch@gmail.com>
2024-09-15 23:10:51 +0800

    pizza项目部分补充

--- 02-pizza-menu/src/index.js
+++ 02-pizza-menu/src/index.js
@@ -103,14 +103,15 @@ function Footer() {
 }
 
 function Pizza(props) {
-  if (props.pizzaObj.soldOut) return null;
-
   return (
-    <li className="pizza">
+    <li className={`pizza ${props.pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
         <h3>{props.pizzaObj.name}</h3>
         <p>{props.pizzaObj.ingredients}</p>
+        <span>
+          {props.pizzaObj.soldOut ? "Sold Out" : props.pizzaObj.price}
+        </span>
       </div>
     </li>
   );
