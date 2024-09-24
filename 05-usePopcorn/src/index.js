8753d73364e1154cd754b1763c2f02227b85509f
lelouchZhang <zhch@gmail.com>
2024-09-24 22:27:38 +0800

    练习useEffect

--- 05-usePopcorn/src/index.js
+++ 05-usePopcorn/src/index.js
@@ -1,25 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
-
-function Test() {
-  const [movieRating, setMovieRating] = React.useState(0);
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+import "./index.css";
+import AppV1 from "./AppV1";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} messages={["D", "C", "B", "A", "S"]} />
-    <StarRating maxRating={10} color="red" defaultRating={3} />
-    <Test />
+    <AppV1 />
   </React.StrictMode>
 );
